d48bfd7441681335ff069f6750c4057f63d9d5ad
Igarashi <gonnbe1106@yahoo.co.jp>
2019-12-29 10:24:40 +0900

    initial commit

---
+++ todoWithFirebase/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  todoWithFirebase
+//
+//  Created by 五十嵐伸雄 on 2019/12/28.
+//  Copyright © 2019 五十嵐伸雄. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet var emailTF: UITextField!
+    @IBOutlet var passwordTF: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func registerAction(_ sender: Any) {
+    }
+    @IBAction func signIn(_ sender: Any) {
+    }
+    
+}
+
